798be192ef6f787b4bfec6239308b1759f89be18
neevpenkar <79773661+neevpenkar@users.noreply.github.com>
2022-12-22 13:37:11 +0200

    session 101

--- Homework 4_3.2/DataFile.cpp
+++ Homework 4_3.2/DataFile.cpp
@@ -1,3 +1,11 @@
 #include "DataFile.h"
 
+DataFile::DataFile(string name, string data):AD_File(name)
+{
+	this->Data = data;
+}
 
+bool DataFile::operator==(const DataFile& f) const
+{
+	return AD_File::operator==(f) && (this->Data == f.Data);
+}
--- Homework 4_3.2/DataFile.h
+++ Homework 4_3.2/DataFile.h
@@ -3,5 +3,13 @@
 class DataFile :
     public AD_File
 {
+    protected:
+        string Data;
+    public:
+        DataFile(string name, string data) /*throw(string)*/;
+
+        /*unsigned*/ int getSize() const { return this->Data.length(); }
+
+        bool operator == (const DataFile& f) const;
 };
 
